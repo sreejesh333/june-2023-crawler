e5ddf82af8d3bd380c5b8c92c4445cb40596d2fb
sreejesh333 <sreejeshravindran523@gmail.com>
2023-06-24 00:34:30 +0530

    tracks and lyrics displayed

--- fe/src/App.js
+++ fe/src/App.js
@@ -7,36 +7,82 @@ import axios, * as others from 'axios';
 
 function App() {
     const [artists, setArtists] = useState([]);
+    const [tracks, setTracks] = useState([]);
+    const [lyrics, setLyrics] = useState([]);
 
     useEffect(() => {
         axios.get("http://127.0.0.1:8000//api/v1/artist")
             .then((resp) => {
                 setArtists(resp.data.artists);
-            });
-        },[]);
-    
-  return (
-          <div className="row">
-          <div className="col">
-          <h2> Artists </h2>
-          <ol>
-                          {artists.map(((artist, idx)=><li key={`artist${artist.id}`}>
-                                        <a 
-                                        href={`http://127.0.0.1:8000/api/v1/artist/${artist.id}`}
+                setTracks([])
+                setLyrics([])
 
-                                        >{artist.name}
-                                        </a>
-                                        </li>))}
-          </ol>
-          </div>
-          <div className="col">
-          <h2> Tracks </h2>
-          </div>
-          <div className="col">
-          <h2> Lyrics </h2>
-          </div>
-          </div>
-  );
+            });
+    }, []);
+
+    function onClickHandlerTracks(e) {
+        e.preventDefault();
+        const artistId = e.currentTarget.getAttribute('artist_id');
+        axios.get(`http://127.0.0.1:8000/api/v1/artist/${artistId}`)
+            .then((resp) => {
+                setTracks(resp.data.tracks);
+                setLyrics([])
+                
+            });
+    }
+
+
+    function onClickHandlerLyrics(e) {
+        e.preventDefault()
+        const trackId = e.currentTarget.getAttribute('track_id')
+        console.log(trackId)
+        axios.get(`http://127.0.0.1:8000/api/v1/song/${trackId}`)
+            .then((resp) => {
+                setLyrics([resp.data])
+                console.log(resp.data)
+            })
+    }
+
+
+
+    return (
+        <div className="row">
+        <div className="col">
+            <h2> Artists </h2>
+            <ol>
+                {artists.map(((artist, idx) => <li key={`artist${artist.id}`}>
+                    <a
+                        href={`http://127.0.0.1:8000/api/v1/artist/${artist.id}`}
+                        onClick={onClickHandlerTracks}
+                        artist_id={artist.id}
+                    >{artist.name}
+                    </a>
+                </li>))}
+            </ol>
+        </div>
+            <div className="col">
+                <h2> Tracks </h2>
+                <ul>
+                    {tracks.map(((track, idx) => <li key={`track${track.id}`}>
+                        <a
+                            href={`http://127.0.0.1:8000/api/v1/song/${track.id}`}
+                            onClick={onClickHandlerLyrics}
+                            track_id={track.id}
+                        >{track.name}
+                        </a>
+                    </li>))}
+                </ul>
+            </div>
+            <div className="col">
+            <h2> Lyrics </h2>
+                {lyrics.map(((lyric, idx) => 
+                <div key={idx}>
+                    <div>{lyric.name}</div>
+                    <div style={{ whiteSpace: 'pre-line' }}>{lyric.lyrics}</div>
+                </div>))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
